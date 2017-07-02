43c1392759db59374c0645f4db8c79c0014fd76e
Scott Willeke <scott@willeke.com>
2017-07-01 21:59:52 -0700

    remove rollup bundling from server - not needed with recent nodejs

--- server/rollup.config.js
+++
@@ -1,6 +0,0 @@
-// NOTE: rollup technically isn't needed here as node supports almost all of ES6 except the module import syntax (https://medium.com/the-node-js-collection/an-update-on-es6-modules-in-node-js-42c958b890c) but it is useful for running the ES6 code via mocha. See https://github.com/rollup/rollup/issues/873#issuecomment-243595904
-export default {
-  entry: 'handler.js',
-  format: 'cjs',
-  dest: 'bundle.js' // equivalent to --output
-};
