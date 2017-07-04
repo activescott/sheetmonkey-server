7b4d087e3e16db33ab8778f75a8e04fde78f6083
Scott Willeke <scott@willeke.com>
2017-07-03 21:14:17 -0700

    support for default mimetype (fixed map files) fixed style issue in profilebutton (no longer blue link) better use of events in svelte

--- server/lib/handlers/StaticFileHandler.js
+++ server/lib/handlers/StaticFileHandler.js
@@ -1,62 +1,65 @@
-'use strict';
-const Promise = require('bluebird');
-const fs = require('fs');
-const path = require('path');
-const Handler = require('./Handler');
-const Mustache = require('mustache');
-const JwtHandler = require('./JwtHandler');
-const Diag = require('../diag');
+'use strict'
+const Promise = require('bluebird')
+const fs = require('fs')
+const path = require('path')
+const Handler = require('./Handler')
+const Mustache = require('mustache')
+const JwtHandler = require('./JwtHandler')
+const Diag = require('../diag')
 
-const D = new Diag('StaticFileHandler');
+const D = new Diag('StaticFileHandler')
 
-Promise.promisifyAll(fs);
+Promise.promisifyAll(fs)
 
 class StaticFileHandler extends Handler {
   /**
-   * 
+   *
    * @param {*string} clientFilesPath The fully qualified path to the client files that this module should serve.
    */
-  constructor(clientFilesPath) {
-    super();
+  constructor (clientFilesPath) {
+    super()
     if (clientFilesPath == null || clientFilesPath.length == 0) {
-      throw new Error('clientFilesPath must be specified');
+      throw new Error('clientFilesPath must be specified')
     }
-    this.clientFilesPath = clientFilesPath;
+    this.clientFilesPath = clientFilesPath
   }
 
-  static getMimeType(filePath) {
+  static getMimeType (filePath) {
     const typeMap = {
-          'html': 'text/html',
-          'css': 'text/css',
-          'js': 'application/javascript',
-          'png': 'image/png',
-          'jpg': 'image/jpeg',
-          'jpeg': 'image/jpeg',
-          'gif': 'image/gif',
-          'json': 'application/json',
-          'xml': 'application/xml',
-          'zip': 'application/zip',
-          'pdf': 'application/pdf',
-          'mp4': 'audio/mpeg',
-          'm4a': 'audio/mpeg',
-          'ico': 'image/x-icon'
-        };
-    let parts = filePath.split('.');
-    var mimeType = '';
-    if (parts.length > 0) {
-      let extension = parts[parts.length-1];
-      if (typeMap.hasOwnProperty(extension))
-        mimeType = typeMap[extension];
+      'html': 'text/html',
+      'css': 'text/css',
+      'js': 'application/javascript',
+      'png': 'image/png',
+      'jpg': 'image/jpeg',
+      'jpeg': 'image/jpeg',
+      'gif': 'image/gif',
+      'json': 'application/json',
+      'xml': 'application/xml',
+      'zip': 'application/zip',
+      'pdf': 'application/pdf',
+      'mp4': 'audio/mpeg',
+      'm4a': 'audio/mpeg',
+      'ico': 'image/x-icon'
     }
-    return mimeType;
+    let parts = filePath.split('.')
+    var mimeType = ''
+    if (parts.length > 0) {
+      let extension = parts[parts.length - 1]
+      if (typeMap.hasOwnProperty(extension)) {
+        mimeType = typeMap[extension]
+      } else {
+        mimeType = 'application/octet-stream' // https://stackoverflow.com/questions/20508788/do-i-need-content-type-application-octet-stream-for-file-download#20509354
+      }
+    }
+    return mimeType
   }
-  get(event, context, callback) {
+  get (event, context, callback) {
     return Promise.try(() => {
       if (!event) {
-        throw new Error('event object not specified.');
+        throw new Error('event object not specified.')
       }
       if (!event.path) {
-        throw new Error('Empty path.');
+        throw new Error('Empty path.')
       }
       // NOTE: We're not enforcing/validate a prefix path for content on the public endpoint here. Instead the serverless.yml is entirely responsible for only routing requests here that should be valid content.
       /*
@@ -65,46 +68,46 @@ class StaticFileHandler extends Handler {
         return reject(new Error('Not a client path.'));
       }
       */
-      const prefix = '';
-      let postfix = event.path.substring(prefix.length); 
-      let basePath = this.clientFilesPath;
-      let filePath = path.join(basePath, postfix);
+      const prefix = ''
+      let postfix = event.path.substring(prefix.length)
+      let basePath = this.clientFilesPath
+      let filePath = path.join(basePath, postfix)
       return StaticFileHandler.readFileAsResponse(filePath, {}, event, context).then(response => {
-        return response;
+        return response
       }).catch(err => {
-        throw new Error(`Unable to read client file '${postfix}'. Error: ${err}`);
-      });
-    });
+        throw new Error(`Unable to read client file '${postfix}'. Error: ${err}`)
+      })
+    })
   }
 
   /**
    * Loads the specified file's content and returns a response that can be called back to lambda for sending the file as the http response.
    */
-  static readFileAsResponse(filePath, viewData) {
+  static readFileAsResponse (filePath, viewData) {
     return fs.readFileAsync(filePath).then(stream => {
-      let mimeType = StaticFileHandler.getMimeType(filePath);
+      let mimeType = StaticFileHandler.getMimeType(filePath)
       if (!mimeType) {
-        let msg = 'Unrecognized MIME type for file ' + filePath;
-        D.error(msg);
-        throw new Error(msg);
+        let msg = 'Unrecognized MIME type for file ' + filePath
+        D.error(msg)
+        throw new Error(msg)
       }
-      let file = stream.toString('utf8');
+      let file = stream.toString('utf8')
       if (!viewData) {
-        viewData = {};
+        viewData = {}
       }
-      viewData.csrftoken = JwtHandler.newToken();
-      file = Mustache.render(file, viewData);
-      
+      viewData.csrftoken = JwtHandler.newToken()
+      file = Mustache.render(file, viewData)
+
       let response = {
         statusCode: 200,
         headers: {
-          "Content-Type": mimeType,
+          'Content-Type': mimeType
         },
         body: file
-      };
-      return response;
-    });
+      }
+      return response
+    })
   }
 }
 
-module.exports = StaticFileHandler;
+module.exports = StaticFileHandler
--- server/test/tests/StaticFileHandler.js
+++ server/test/tests/StaticFileHandler.js
@@ -1,34 +1,45 @@
-'use strict';
-require('../support/setup.js');
-const expect = require("chai").expect;
+/* eslint-env mocha */
+/* eslint-disable padded-blocks */
+'use strict'
+require('../support/setup.js')
+const expect = require('chai').expect
 
-const path = require('path');
+const path = require('path')
 
-const StaticFileHandler = require('../../lib/handlers/StaticFileHandler.js');
+const StaticFileHandler = require('../../lib/handlers/StaticFileHandler.js')
 
-describe('StaticFileHandler', function() {
-  describe('constructor', function() {
+describe('StaticFileHandler', function () {
+  describe('constructor', function () {
+    it('should not allow empty arg', function () {
+      expect(() => new StaticFileHandler()).to.throw(/clientFilesPath must be specified/)
+    })
 
-    it('should not allow empty arg', function() {
-      expect(() => new StaticFileHandler()).to.throw(/clientFilesPath must be specified/);
-    });
-
-    it('should accept string arg', function() {
-      expect(() => new StaticFileHandler('some/path')).to.not.throw(Error);
-    });
-    
-  });
-
-  describe('get', function() {
-
-    it('should return index.html', function() {
-        let event = { path: 'index.html' };
-        let h = new StaticFileHandler(path.join(__dirname, '../../data/public/'));
-        let response = h.get(event, null);
-        expect(response).to.eventually.have.property('statusCode', 200);
-        expect(response).to.eventually.have.property('body').to.match(/^<\!DOCTYPE html>/);
-        return response;
-    });
-
+    it('should accept string arg', function () {
+      expect(() => new StaticFileHandler('some/path')).to.not.throw(Error)
+    })
   })
-});
+
+  describe('get', function () {
+    it('should return index.html', function () {
+      let event = { path: 'index.html' }
+      let h = new StaticFileHandler(path.join(__dirname, '../../data/public/'))
+      let response = h.get(event, null)
+      expect(response).to.eventually.have.property('statusCode', 200)
+      expect(response).to.eventually.have.property('body').to.match(/^<!DOCTYPE html>/)
+      return response
+    })
+
+    it('should return a mime/type of application/octet-stream for .map files', function () {
+      let event = { path: 'vendor/bootstrap.min.css.map' }
+      let h = new StaticFileHandler(path.join(__dirname, '../../data/public/'))
+      return h.get(event, null)
+        .then(response => {
+          console.log('response.headers', response.headers)
+          expect(response).to.have.property('headers')
+          expect(response.headers).to.have.property('Content-Type')
+          expect(response.headers['Content-Type']).to.equal('application/octet-stream')
+          return response
+        })
+    })
+  })
+})
